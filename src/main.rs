0037fe325c4c8a08e8c5783ece7d2577b40ad5b1
Roman Muzikantov <12068940+LilCurly@users.noreply.github.com>
2019-12-02 19:19:50 +0100

    Added new structure to keep the query and the filename together

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,21 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
     let filename = &args[2];
 
     println!("query : {:?}, filename : {:?}", query, filename);
 
-    let file_content = fs::read_to_string(filename).expect(format!("failed to read file : '{}'", filename).as_str());
+    let file_content = fs::read_to_string(filename)
+        .expect(format!("failed to read file : '{}'", filename).as_str());
 
     println!("Content of the file :\n{}", file_content);
+}
+
+#[derive(Debug)]
+pub struct Config {
+    query: String,
+    filename: String
 }
