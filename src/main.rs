3538fa39a9988ce0a105ec07ffb62da3f1ac3115
Roman Muzikantov <12068940+LilCurly@users.noreply.github.com>
2019-12-02 23:38:42 +0100

    Moved the logic into lib.rs to make it testable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use grep_rs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,29 +13,8 @@ fn main() {
 
     println!("query : {:?}, filename : {:?}", config.query, config.filename);
 
-    let file_content = fs::read_to_string(&config.filename)
-        .expect(format!("failed to read file : '{}'", &config.filename).as_str());
-
-    println!("Content of the file :\n{}", file_content);
-}
-
-#[derive(Debug)]
-pub struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments.");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config {
-            query,
-            filename
-        })
+    if let Err(e) = grep_rs::run(config) {
+        println!("Error occured while running the program: {}", e);
+        process::exit(1);
     }
 }
