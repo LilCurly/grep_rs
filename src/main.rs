7c0a7f5d839c0f215670bda80fe20a3eb534fb43
Roman Muzikantov <12068940+LilCurly@users.noreply.github.com>
2019-12-02 22:49:58 +0100

    Handling error by returning a Result from the new constructor method -> Ok if it receives at least 3 arguments Err if it does not

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error while parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("query : {:?}, filename : {:?}", config.query, config.filename);
 
@@ -21,16 +25,16 @@ pub struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments, should be 2 but got {}", args.len() - 1);
+            return Err("Not enough arguments.");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config {
+        Ok(Config {
             query,
             filename
-        }
+        })
     }
 }
