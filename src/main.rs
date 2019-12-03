f4db97738a93fd05f71b101f7c319326575f9c58
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-12-03 15:45:07 +0100

    Redirecting the errors to the standard error output

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Error while parsing arguments: {}", err);
+        eprintln!("Error while parsing arguments: {}", err);
         process::exit(1);
     });
 
     println!("query : {:?}, filename : {:?}", config.query, config.filename);
 
     if let Err(e) = grep_rs::run(config) {
-        println!("Error occured while running the program: {}", e);
+        eprintln!("Error occured while running the program: {}", e);
         process::exit(1);
     }
 }
